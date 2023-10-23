fc02a1b4c6b868d6820db52f54023d39c0de15b3
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-23 16:29:55 +0530

    creating routing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
+};
+
+export default layout;
---
+++ app/(root)/ask-questions/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Ask -questions/</h1>
+    </div>
+  );
+}
