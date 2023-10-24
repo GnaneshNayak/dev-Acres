92909ed24f67b335fb985915e96dee6c74087832
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-24 14:44:11 +0530

    added clerk authentication

--- app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const SignUp = (props: Props) => {
-  return <div>sign-up</div>;
-};
-
-export default SignUp;
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">hello next js 13</h1>
-      <h2 className="h2-bold">hello next js 13</h2>
-      <h3 className="h3-bold">hello next js 13</h3>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
