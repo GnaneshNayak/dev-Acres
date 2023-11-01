231b32c115340f15bedf44a27d7134c823cc03de
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-11-01 21:31:51 +0530

    fixing firstname bug

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -79,7 +79,8 @@ export async function POST(req: Request) {
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
-        name: `${first_name} + ${last_name ? ` ${last_name}` : ''}`,
+        name: `${first_name}${last_name ? ` ${last_name}` : ''}`,
+
         username: username!,
         email: email_addresses[0].email_address,
         picture: image_url,
