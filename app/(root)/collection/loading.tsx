f372d7a4ea80347d71598884f4f6bdd8ae55736a
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-11-25 13:38:00 +0530

    created lading file for the tags and the profile pge

---
+++ app/(root)/collection/loading.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import React from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const Loading = () => {
+  return (
+    <section>
+      <div className=" flex w-full justify-between gap-4 sm:flex-row sm:items-center">
+        <h1
+          className="h1-bold text-dark100_light900
+        "
+        >
+          Saved Questions
+        </h1>
+      </div>
+      <div
+        className="mb-12 mt-11 flex flex-wrap items-center justify-between 
+    gap-5"
+      >
+        <Skeleton className="h-14 flex-1" />
+        <div className="hidden max-md:block">
+          <Skeleton className="h-14 w-28" />
+        </div>
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <Skeleton className="h-48 w-full rounded-xl" />
+        <Skeleton className="h-48 w-full rounded-xl" />
+        <Skeleton className="h-48 w-full rounded-xl" />
+        <Skeleton className="h-48 w-full rounded-xl" />
+        <Skeleton className="h-48 w-full rounded-xl" />
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
